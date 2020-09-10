f4ffd033f290f5df9968d5bec1bd1fb7b10e2c6a
koi <kkoiwa.1720+git@gmail.com>
2020-09-10 17:47:50 +0900

    for chapter 7

--- common/layers.py
+++ common/layers.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from common.functions import softmax, cross_entropy_error
+from common.util import im2col, col2im
 
 
 class Relu:
@@ -40,17 +41,46 @@ class Sigmoid:
         return dx
 
 
+# class Affine:
+#     def __init__(self, W, b):
+#         self.W = W
+#         self.b = b
+#         self.x = None
+#         self.dW = None
+#         self.db = None
+
+#     def forward(self, x):
+#         self.x = x
+#         out = np.dot(x, self.W) + self.b
+
+#         return out
+
+#     def backward(self, dout):
+#         dx = np.dot(dout, self.W.T)
+#         self.dW = np.dot(self.x.T, dout)
+#         self.db = np.sum(dout, axis=0)
+
+#         return dx
+
+
 class Affine:
     def __init__(self, W, b):
-        self.W = W
+        self.W =W
         self.b = b
+
         self.x = None
+        self.original_x_shape = None
+        # 重み・バイアスパラメータの微分
         self.dW = None
         self.db = None
 
     def forward(self, x):
+        # テンソル対応
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
         self.x = x
-        out = np.dot(x, self.W) + self.b
+
+        out = np.dot(self.x, self.W) + self.b
 
         return out
 
@@ -59,6 +89,7 @@ class Affine:
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
 
+        dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
         return dx
 
 
@@ -108,7 +139,7 @@ class SoftmaxWithLoss:
 
 
 class Dropout:
-    def __init_+_(self, dropout_ratio=0.5):
+    def __init__(self, dropout_ratio=0.5):
         self.dropout_ratio = dropout_ratio
         self.mask = None
 
@@ -121,3 +152,166 @@ class Dropout:
 
     def backward(self, dout):
         return dout * self.mask
+
+
+# class Convolution:
+#     def __init__(self, W, b, stride=1, pad=0):
+#         self.W = W
+#         self.b = b
+#         self.stride = stride
+#         self.pad = pad
+
+#     def forward(self, x):
+#         FN, C, FH, FW = self.W.shape
+#         N, C, H, W = x.shape
+#         out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+#         out_w = int(1 + (W + 2 * self.pad - FW) / self.stride)
+
+#         col = im2col(x, FH, FW, self.stride, self.pad)
+#         col_W = self.W.reshape(FN, -1).T
+#         out = np.dot(col, col_W) + self.b
+
+#         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+#         return out
+
+#     def backward(self, dout):
+#         FN, C, FH, FW = self.W.shape
+#         dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+#         self.db = np.sum(dout, axis=0)
+#         self.dW = np.dot(self.col.T, dout)
+#         self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+#         dcol = np.dot(dout, self.col_W.T)
+#         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+#         return dx
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+        # 中間データ（backward時に使用）
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+
+
+# class Pooling:
+#     def __init__(self, pool_h, pool_w, stride=1, pad=0):
+#         self.pool_h = pool_h
+#         self.pool_w = pool_w
+#         self.stride = stride
+#         self.pad = pad
+
+#     def forward(self, x):
+#         N, C, H, W = x.shape
+#         out_h = int(1 + (H - self.pool_h) / self.stride)
+#         out_w = int(1 + (W - self.pool_w) / self.stride)
+
+#         # 展開
+#         col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+#         col = col.reshape(-1, self.pool_h * self.pool_w)
+
+#         # 最大値
+#         out = np.max(col, axis=1)
+
+#         # 整形
+#         out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+#         return out
+
+#     def backward(self, dout):
+#         dout = dout.transpose(0, 2, 3, 1)
+
+#         pool_size = self.pool_h * self.pool_w
+#         dmax = np.zeros((dout.size, pool_size))
+#         dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+#         dmax = dmax.reshape(dout.shape + (pool_size,))
+
+#         dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+#         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+
+#         return dx
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+
+        return dx
