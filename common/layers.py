6f39be566cfb46daa81e1ca3349bf37c02dc919f
koi <kkoiwa.1720+git@gmail.com>
2020-09-10 15:48:21 +0900

    for chapter 6

--- common/layers.py
+++ common/layers.py
@@ -62,11 +62,31 @@ class Affine:
         return dx
 
 
+# class SoftmaxWithLoss:
+#     def __init__(self):
+#         self.loss = None
+#         self.y = None
+#         self.t = None
+
+#     def forward(self, x, t):
+#         self.t = t
+#         self.y = softmax(x)
+#         self.loss = cross_entropy_error(self.y, self.t)
+
+#         return self.loss
+
+#     def backward(self, dout=1):
+#         batch_size = self.t.shape[0]
+#         dx = (self.y - self.t) / batch_size
+
+#         return dx
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.loss = None
-        self.y = None
-        self.t = None
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ
 
     def forward(self, x, t):
         self.t = t
@@ -77,6 +97,27 @@ class SoftmaxWithLoss:
 
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
-        dx = (self.y - self.t) / batch_size
+        if self.t.size == self.y.size: # 教師データがone-hot-vectorの場合
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
 
         return dx
+
+
+class Dropout:
+    def __init_+_(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
